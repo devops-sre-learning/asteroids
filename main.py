1b579aea2f7ed168a78e8305e0f0e1f8c1370155
devops-sre-learning <ryan.lukasik@gmail.com>
2025-08-08 10:40:15 -0500

    Asteroids

---
+++ main.py
@@ -0,0 +1,17 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
