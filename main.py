146e39e6365fb6560d02ba0e4f8fcd1fa54d8764
devops-sre-learning <ryan.lukasik@gmail.com>
2025-08-09 14:47:13 -0500

    added screen

--- main.py
+++ main.py
@@ -4,13 +4,30 @@
 import pygame
 from constants import *
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+    
+
+    
+
+#def fill(color):
+    
+
+
 
 
 if __name__ == "__main__":
